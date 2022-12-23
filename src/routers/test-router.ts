0a4dcfa7e6f810d84695bf241edb39045323a48e
Renat <jeingo20@gmail.com>
2022-12-23 23:35:43 +0300

    add test service

--- src/routers/test-router.ts
+++ src/routers/test-router.ts
@@ -1,10 +1,10 @@
 import {Router, Request, Response} from 'express'
 import {HTTP_STATUSES} from "../constats/status"
-import {testRepository} from "../repositories/test-repository"
+import {testService} from "../domain/test-service"
 
 export const testRouter = Router({})
 
 testRouter.delete('/', async (req: Request, res: Response) => {
-    await testRepository.deleteAllDB()
+    await testService.deleteAllDB()
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
