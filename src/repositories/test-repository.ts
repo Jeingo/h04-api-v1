191bbdfa2aa18b562a37c01c92c0e13fc2db8d00
Renat <jeingo20@gmail.com>
2022-12-27 23:51:19 +0300

    add deleting data in users BD

--- src/repositories/test-repository.ts
+++ src/repositories/test-repository.ts
@@ -1,8 +1,9 @@
-import {blogsCollection, postsCollection} from "./db"
+import {blogsCollection, postsCollection, usersCollection} from "./db"
 
 export const testRepository = {
     async deleteAllDB() {
         await blogsCollection.deleteMany({})
         await postsCollection.deleteMany({})
+        await usersCollection.deleteMany({})
     }
 }
